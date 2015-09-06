b312c822437ddfad0bc3ac2bd8e77a58bdd9f144
karel.wintersky <karel.wintersky@gmail.com>
2015-09-06 23:24:55 +0300

    0.5.13 Added PHPAuth Library

---
+++ backend/phpauth/config.class.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * PHPAuth config class
+ */
+namespace PHPAuth;
+
+class Config
+{
+    private $dbh;
+    private $config;
+    private $phpauth_config_table = 'lme_phpauth_config';
+
+    /**
+     * @param \PDO $dbh
+     */
+    public function __construct(\PDO $dbh)
+    {
+        $this->dbh = $dbh;
+
+        $this->config = array();
+
+        $query = $this->dbh->prepare("SELECT * FROM {$this->phpauth_config_table}");
+        $query->execute();
+
+        while($row = $query->fetch()) {
+            $this->config[$row['setting']] = $row['value'];
+        }
+    }
+
+    /**
+     * @param $setting
+     * @return mixed
+     */
+    public function __get($setting)
+    {
+        return $this->config[$setting];
+    }
+
+    /**
+     * @param $setting
+     * @param $value
+     * @return bool
+     */
+    public function __set($setting, $value)
+    {
+        $query = $this->dbh->prepare("UPDATE {$this->phpauth_config_table} SET value = ? WHERE setting = ?");
+
+        if($query->execute(array($value, $setting))) {
+            $this->config[$setting] = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ backend/phpauth/languages/ru_RU.php
@@ -0,0 +1,70 @@
+<?php
+
+$lang = array();
+
+$lang['user_blocked'] = "You are currently locked out of the system.";
+
+$lang['email_password_invalid'] = "Недопустимые E-Mail или пароль.";
+$lang['email_password_incorrect'] = "Пользователь с указанным E-Mail'ом не обнаружен в системе или пароль не подходит.";
+$lang['remember_me_invalid'] = "Недопустимое значение поля 'запомнить пользователя'.";
+
+$lang['password_short'] = "Пароль слишком короткий.";
+$lang['password_long'] = "Пароль слишком длинный.";
+$lang['password_invalid'] = "Пароль должен содержать хотя бы одну цифру, хотя бы одну строчную букву и хотя бы одну прописную.";
+$lang['password_nomatch'] = "Пароли не совпадают.";
+$lang['password_changed'] = "Пароль успешно изменен.";
+$lang['password_incorrect'] = "Текущий пароль указан неверно.";
+$lang['password_notvalid'] = "Недопустимый пароль.";
+
+$lang['newpassword_short'] = "Новый пароль слишком короткий.";
+$lang['newpassword_long'] = "Новый пароль слишком длинный.";
+$lang['newpassword_invalid'] = "Новый пароль должен содержать хотя бы одну цифру, хотя бы одну строчную букву и хотя бы одну прописную..";
+$lang['newpassword_nomatch'] = "Новые пароли не совпадают.";
+$lang['newpassword_match'] = "Новый пароль такой же, как старый.";
+
+$lang['email_short'] = "Адрес E-Mail слишком короткий.";
+$lang['email_long'] = "Адрес E-Mail слишком длинный";
+$lang['email_invalid'] = "Недопустимый E-Mail.";
+$lang['email_incorrect'] = "E-Mail неверен.";
+$lang['email_banned'] = "Этот E-Mail запрещен.";
+$lang['email_changed'] = "E-Mail изменен успешно.";
+
+$lang['newemail_match'] = "Новый E-Mail сопадает со старым.";
+
+$lang['account_inactive'] = "Аккаунт еще не активирован.";
+$lang['account_activated'] = "Аккаунт активирован.";
+
+$lang['logged_in'] = "Вы вошли в систему.";
+$lang['logged_out'] = "Вы вышли из системы.";
+
+$lang['system_error'] = "Произошла системная ошибка (проблема с сессией). Попробуйте снова.";
+
+$lang['register_success'] = "Учётная запись создана. На вашу почту отправлены инструкции по активации.";
+$lang['email_taken'] = "Этот E-Mail уже используется!.";
+
+$lang['resetkey_invalid'] = "Reset key is invalid.";
+$lang['resetkey_incorrect'] = "Reset key is incorrect.";
+$lang['resetkey_expired'] = "Reset key has expired.";
+$lang['password_reset'] = "Пароль сброшен успешно.";
+
+$lang['activationkey_invalid'] = "Недопустимый ключ акцивации учётной записи.";
+$lang['activationkey_incorrect'] = "Неверный ключ акцивации учётной записи.";
+$lang['activationkey_expired'] = "Срок действия ключа активации истёк..";
+
+$lang['reset_requested'] = "Password reset request sent to email address.";
+$lang['reset_exists'] = "Сброс пароля уже запрошен.";
+
+$lang['already_activated'] = "Учетная запись уже активирована.";
+$lang['activation_sent'] = "Сообщение с инструкциями по активации учетной записи выслано.";
+$lang['activation_exists'] = "Мы уже высылали вам сообщение с инструкциями по активации учетной записи.";
+
+$lang['email_activation_subject'] = "%s - Активировать учётную запись";
+$lang['email_activation_body'] = "Hello,<br/><br/> To be able to log in to your account you first need to activate your account by clicking on the following link : <strong><a href=\"%1$s/%2$s\">%1$s/%2$s</a></strong><br/><br/> You then need to use the following activation key: <strong>%3$s</strong><br/><br/> If you did not sign up on %1$s recently then this message was sent in error, please ignore it.";
+$lang['email_activation_altbody'] = "Hello, \n\n To be able to log in to your account you first need to activate your account by visiting the following link :\n %1$s/%2$s\n\n You then need to use the following activation key: %3$s\n\n If you did not sign up on %1$s recently then this message was sent in error, please ignore it.";
+
+$lang['email_reset_subject'] = "%s - Запрос сброса пароля";
+$lang['email_reset_body'] = "Здравствуйте,<br/><br/>Для сброса вашего пароля пройдите пожалуйста по этой ссылке:<br/><br/><strong><a href=\"%1$s/%2$s\">%1$s/%2$s</a></strong><br/><br/>Вам нужно будет использовать следующий ключ для сброса пароля: <strong>%3$s</strong><br/><br/>Если вы недавно не запрашивали сброс пароля на сайте %1$s, значит это сообщение вы получили по ошибке. Пожалуйста, проигнорируйте его.";
+$lang['email_reset_altbody'] = "Здравствуйте, \n\n Для сброса вашего пароля пройдите пожалуйста по этой ссылке:\n %1$s/%2$s\n\n Вам нужно будет использовать следующий ключ для сброса пароля: %3$s\n\n Если вы недавно не запрашивали сброс пароля на сайте %1$s, значит это сообщение вы получили по ошибке. Пожалуйста, проигнорируйте его.";
+
+$lang['account_deleted'] = "Учётная запись удалена.";
+
